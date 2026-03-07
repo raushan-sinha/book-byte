cd03b6f4582457b6865cf07213e0d11afadcec1a
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-03-07 13:44:38 +0530

    Created logics for Search Bar function

---
+++ src/components/layout/navbar/SearchBar.tsx
@@ -0,0 +1,38 @@
+import type React from "react";
+import { useState } from "react";
+import { BiSearch } from "react-icons/bi";
+
+const SearchBar: React.FC = () => {
+    const [query, setQuery] = useState<string>('');
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log('Searching for:', query);
+    }
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+
+        if (/^[a-zA-Z\s]*$/.test(value)) {
+            setQuery(value);
+        }
+    }
+
+    return (
+        <form onSubmit={handleSubmit} className="flex items-center w-full max-w-xl bg-white border border-cyan-400 rounded-full px-4 py-2 shadow-sm focus-within:ring-2 focus-within:ring-blue-500 transition">
+            <BiSearch className="text-black mr-2" size={25} />
+
+            <input
+                type="text"
+                id="search"
+                name="search"
+                autoComplete="off"
+                placeholder="Search books, authors..."
+                value={query}
+                onChange={handleChange}
+                className="w-full bg-transparent outline-none text-gray-800 placeholder-gray-400 text-sm"
+            />
+        </form>
+    );
+}
+
+export default SearchBar;
