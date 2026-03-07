6fea4cdafd15f27c7f1cf303bb95b82eedb8eb7d
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-03-07 13:45:15 +0530

    Code updated with new styles & components

--- src/components/layout/navbar/Navbar.tsx
+++ src/components/layout/navbar/Navbar.tsx
@@ -1,9 +1,20 @@
 import Logo from "./Logo";
+import SearchBar from "./SearchBar";
 
 export default function Navbar() {
     return (
-        <nav className="flex items-center justify-between px-8 py-4 shadow">
-            <Logo />
+        <nav className="flex flex-wrap items-center justify-between gap-4 px-4 sm:px-6 lg:px-8 py-4 shadow-md bg-white">
+
+            {/* Logo */}
+            <div className="flex-shrink-0">
+                <Logo />
+            </div>
+
+            {/* Search */}
+            <div className="w-full sm:w-auto sm:flex-1 flex justify-center">
+                <SearchBar />
+            </div>
+
         </nav>
     );
 }
