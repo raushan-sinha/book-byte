726051cd0cdfdf10bf952cca7c9e9f5e2642e0c3
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-03-08 20:33:03 +0530

    Stored the data of Links

---
+++ src/components/layout/navbar/navLinksData.ts
@@ -0,0 +1,10 @@
+import type { NavLinkItem } from "./types";
+
+export const navLinks: NavLinkItem[] = [
+    { name: "Home", path: "/" },
+    { name: "Explore Books", path: "/books" },
+    { name: "Categories", path: "/categories" },
+    { name: "Best Sellers", path: "/best-sellers" },
+    { name: "New Arrivals", path: "/new-arrivals" },
+    { name: "Blog", path: "/blog" }
+];
