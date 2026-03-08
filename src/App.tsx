bebe58040cceaa8f098d2f3f4524051bc544f30c
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-03-08 20:58:40 +0530

    Added React-Router-Dom links & added two components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import type React from "react";
 import { Navbar } from "./components/layout/navbar"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import ExploreBooks from "./pages/ExploreBooks/ExploreBooks";
 
 const App: React.FC = () => {
     return (
-        <Navbar />
+        <BrowserRouter>
+            <header>
+                <Navbar />
+            </header>
+
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/explore-books" element={<ExploreBooks />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
