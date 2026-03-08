564ce463ee848bee7740e060aed67b2b58aac8dc
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-03-08 20:58:00 +0530

    Added 'Link' for Router the page's path

--- src/components/layout/navbar/NavLinks.tsx
+++ src/components/layout/navbar/NavLinks.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { navLinks } from "./navLinksData";
 
 const NavLinks = () => {
@@ -5,9 +6,9 @@ const NavLinks = () => {
         <ul className="hidden lg:flex items-center gap-6 list-none">
             {navLinks.map((link, idx) => (
                 <li key={idx}>
-                    <a className="font-medium text-gray-700 hover:text-black transition cursor-pointer">
+                    <Link to={link.path} className="font-medium text-gray-700 hover:text-black transition cursor-pointer">
                         {link.name}
-                    </a>
+                    </Link>
                 </li>
             ))}
         </ul>
