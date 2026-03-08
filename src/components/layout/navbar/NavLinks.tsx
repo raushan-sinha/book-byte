d6888a5f64d55692dfc466e0a3ca1825d70e11d4
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-03-08 21:05:13 +0530

    Added a 'useLocation' for changing styles in the links after changing the Path

--- src/components/layout/navbar/NavLinks.tsx
+++ src/components/layout/navbar/NavLinks.tsx
@@ -1,12 +1,14 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { navLinks } from "./navLinksData";
 
 const NavLinks = () => {
+    const location = useLocation();
+
     return (
         <ul className="hidden lg:flex items-center gap-6 list-none">
             {navLinks.map((link, idx) => (
                 <li key={idx}>
-                    <Link to={link.path} className="font-medium text-gray-700 hover:text-black transition cursor-pointer">
+                    <Link to={link.path} className={`font-medium text-gray-700 transition cursor-pointer ${location.pathname === link.path ? 'underline underline-offset-4 decoration-2 text-red-600' : ''}`}>
                         {link.name}
                     </Link>
                 </li>
