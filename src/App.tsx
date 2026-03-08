9d867a20e009ad20e9871105615356822aa28e74
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-03-08 21:12:29 +0530

    Added components

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { Navbar } from "./components/layout/navbar"
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import ExploreBooks from "./pages/ExploreBooks/ExploreBooks";
+import Categories from "./pages/Categories/Categories";
+import BestSellers from "./pages/BestSellers/BestSellers";
+import NewArrivals from "./pages/NewArrivals/NewArrivals";
 
 const App: React.FC = () => {
     return (
@@ -14,6 +17,9 @@ const App: React.FC = () => {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/explore-books" element={<ExploreBooks />} />
+                <Route path="/categories" element={<Categories />} />
+                <Route path="/best-sellers" element={<BestSellers />} />
+                <Route path="/new-arrivals" element={<NewArrivals />} />
             </Routes>
         </BrowserRouter>
     );
