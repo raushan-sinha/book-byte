d0b11ac4571d4e177745fc88fd985cf63753a0b9
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-03-12 13:57:46 +0530

    Created a file for User's Authentication for "Login"

---
+++ src/components/layout/navbar/AuthButtons.tsx
@@ -0,0 +1,13 @@
+const AuthButtons = () => {
+    return (
+        <div className="hidden lg:flex items-center gap-3">
+
+            <a className="px-5 py-2 text-sm font-semibold text-white bg-red-600 rounded-md hover:bg-red-700 transition cursor-pointer">
+                Login
+            </a>
+
+        </div>
+    );
+}
+
+export default AuthButtons;
