ac22767bde186a970aef27d5ebf82e4e8a4808ed
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-03-12 14:01:08 +0530

    Added a new component - AuthButton

--- src/components/layout/navbar/Navbar.tsx
+++ src/components/layout/navbar/Navbar.tsx
@@ -3,35 +3,39 @@ import Logo from "./Logo";
 import MobileMenu from "./MobileMenu";
 import NavLinks from "./NavLinks";
 import SearchBar from "./SearchBar";
+import AuthButtons from "./AuthButtons";
 
 export default function Navbar() {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const handleNavigate = () => {
-    navigate("/");
+    const handleNavigate = () => {
+        navigate("/");
 
-    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
-  };
+        window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+    };
 
-  return (
-    <nav className="relative flex flex-wrap items-center justify-between gap-4 px-4 sm:px-6 lg:px-8 py-4 shadow-md bg-white">
-      {/* Logo */}
-      <div className="shrink-0" onClick={handleNavigate}>
-        <Logo />
-      </div>
+    return (
+        <nav className="relative flex flex-wrap items-center justify-between gap-4 px-4 sm:px-6 lg:px-8 py-4 shadow-md bg-white">
+            {/* Logo */}
+            <div className="shrink-0" onClick={handleNavigate}>
+                <Logo />
+            </div>
 
-      {/* Search */}
-      <div className="w-full sm:w-auto sm:flex-1 flex justify-center">
-        <SearchBar />
-      </div>
+            {/* Search */}
+            <div className="w-full sm:w-auto sm:flex-1 flex justify-center">
+                <SearchBar />
+            </div>
 
-      {/* Desktop Nav */}
-      <NavLinks />
+            {/* Desktop Nav */}
+            <NavLinks />
 
-      {/* Mobile Menu */}
-      <div className="absolute top-4 right-4 lg:hidden">
-        <MobileMenu />
-      </div>
-    </nav>
-  );
+            {/* Authbuttons */}
+            <AuthButtons />
+
+            {/* Mobile Menu */}
+            <div className="absolute top-4 right-4 lg:hidden">
+                <MobileMenu />
+            </div>
+        </nav>
+    );
 }
