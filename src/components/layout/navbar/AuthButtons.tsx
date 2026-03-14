ffe1c0b5c3840b2ed8bd6ac3dab32fa6acbb76d0
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-03-14 21:20:15 +0530

    Added sttyles

--- src/components/layout/navbar/AuthButtons.tsx
+++ src/components/layout/navbar/AuthButtons.tsx
@@ -2,11 +2,11 @@ const AuthButtons = () => {
     return (
         <div className="hidden lg:flex items-center gap-3">
 
-            <a className="px-5 py-2 text-sm font-semibold text-white bg-red-600 rounded-md hover:bg-red-700 transition cursor-pointer">
+            <a className="px-5 py-2 text-sm font-semibold text-white bg-red-700 rounded-md hover:bg-red-600 transition cursor-pointer">
                 Login
             </a>
 
-            <a className="px-5 py-2 text-sm font-semibold text-white bg-blue-600 rounded-md hover:bg-red-700 transition cursor-pointer">
+            <a className="px-5 py-2 text-sm font-semibold text-white bg-blue-700 rounded-md hover:bg-blue-600 transition cursor-pointer">
                 Sign Up
             </a>
 
